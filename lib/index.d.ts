ebe117dbdc8cda828937aa796812a57340056898
Mitch Crowe <crowe.mitch@gmail.com>
2017-08-09 19:51:01 -0700

    Cleanup

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,14 @@
-export declare function doSomething(a: number): number;
+/// <reference types="react" />
+import * as React from 'react';
+export default class Connector {
+    private channel;
+    /**
+     * Refresh all wrapped components
+     */
+    refresh(): void;
+    /**
+     * Wrap a React component class
+     * @param component A React component class
+     */
+    wrap(component: React.ComponentClass): React.ComponentClass<{}>;
+}
